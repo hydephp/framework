70dd8df956e7fc1bc1c9b67a14e2b23a8fea4d76
Caen De Silva <caen@desilva.se>
2022-03-22 13:07:10 +0100

    0.3.1 Move commands to framework

---
+++ src/Commands/Validate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hyde\Framework\Commands;
+
+use Hyde\Framework\Hyde;
+use LaravelZero\Framework\Commands\Command;
+
+class Validate extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'validate';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Run a series of tests to validate your setup and help you optimize your site.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Running validation tests!');
+
+        $this->line(shell_exec(Hyde::path('vendor/bin/pest') . ' --group=validators'));
+
+        $this->info('All done!');
+    }
+}
