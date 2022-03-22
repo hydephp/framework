b0593434e22cc9a132302aa3021181b04030841b
Caen De Silva <caen@desilva.se>
2022-03-22 13:21:01 +0100

    0.3.2 Move service provider from App to Hyde

---
+++ src/HydeServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hyde\Framework;
+
+use Hyde\Framework\Actions\CreatesDefaultDirectories;
+use Illuminate\Support\ServiceProvider;
+
+class HydeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        (new CreatesDefaultDirectories)->__invoke();
+    }
+}
