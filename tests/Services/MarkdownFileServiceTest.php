7aaa3ceeeb5e72362b94712fabaee96788005940
Caen De Silva <caen@desilva.se>
2022-03-27 12:45:07 +0200

    Add the test methods

--- tests/Services/MarkdownFileServiceTest.php
+++ tests/Services/MarkdownFileServiceTest.php
@@ -8,4 +8,38 @@ use PHPUnit\Framework\TestCase;
 class MarkdownFileServiceTest extends TestCase
 {
 
+    public function testSplit()
+    {
+
+    }
+
+    public function testGetDocument()
+    {
+
+    }
+
+    public function testParseFrontMatter()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testParseFileStream()
+    {
+
+    }
+
+    public function testGetFilepath()
+    {
+
+    }
+
+    public function testContainsFrontMatter()
+    {
+
+    }
 }
