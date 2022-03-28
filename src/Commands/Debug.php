e0e3334206f330c8bc66de8fabf33cbe060b1b5f
Caen De Silva <caen@desilva.se>
2022-03-28 12:02:05 +0200

    Deprecate class as it will be renamed

--- src/Commands/Debug.php
+++ src/Commands/Debug.php
@@ -5,6 +5,11 @@ namespace Hyde\Framework\Commands;
 use Hyde\Framework\Hyde;
 use LaravelZero\Framework\Commands\Command;
 
+/**
+ * Hyde Command to print debug information.
+ * 
+ * @deprecated 0.6.0 - Class will be renamed to DebugCommand
+ */
 class Debug extends Command
 {
     /**
