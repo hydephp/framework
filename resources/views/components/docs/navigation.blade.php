f6e592a883c9cbb8501aae09e3887b750f43a194
Caen De Silva <caen@desilva.se>
2022-04-01 22:47:55 +0200

    Fix #58

--- resources/views/components/docs/navigation.blade.php
+++ resources/views/components/docs/navigation.blade.php
@@ -1,7 +1,13 @@
 <div class="flex flex-row items-center justify-between h-full px-4">
-	<a href="../docs/index.html" class="font-bold">
-		{{ config('hyde.name') }} Docs
-	</a>
+	<span class="font-bold">
+		@if(Hyde::docsIndexPath() !== false)
+		<a href="{{ basename(Hyde::docsIndexPath()) }}">
+			{{ config('hyde.name') }} Docs
+		</a>
+		@else
+			{{ config('hyde.name') }} Docs
+		@endif
+	</span>
 
 	@include('hyde::components.docs.sidebar-toggle')
 </div>
