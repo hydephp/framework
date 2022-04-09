64fa1f0bb340be6e3a53e7eab9ee76a6a28f9c43
Caen De Silva <caen@desilva.se>
2022-04-09 12:06:58 +0200

    Clean up code

--- src/Commands/HydePublishHomepageCommand.php
+++ src/Commands/HydePublishHomepageCommand.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Artisan;
  */
 class HydePublishHomepageCommand extends Command
 {
-    protected $signature = 'publish:homepage
-                                {homepage? : The name of the page to publish}
+    protected $signature = 'publish:homepage {homepage? : The name of the page to publish}
                                 {--force : Overwrite any existing files}';
 
     protected $description = 'Publish one of the default homepages to index.blade.php.';
@@ -23,86 +22,66 @@ class HydePublishHomepageCommand extends Command
 
     public function handle(): int
     {
-        $this->selected = $this->argument('homepage') ?? $this->promptForFile();
+        $this->selected = $this->argument('homepage') ?? $this->promptForHomepage();
 
-        $status = (new PublishesHomepageView($this->selected, $this->canOverwriteFile()))->execute();
+        $returnValue = (new PublishesHomepageView(
+            $this->selected,
+            $this->canExistingIndexFileBeOverwritten()
+        ))->execute();
 
-        if ($status === true) {
+        if ($returnValue === true) {
             $this->info("Homepage published successfully!");
         } else {
-            if (is_numeric($status)) {
-                if ($status == 404) {
+            if (is_numeric($returnValue)) {
+                if ($returnValue == 404) {
                     $this->error('Homepage ' . $this->selected . ' does not exist.');
                     return 404;
                 }
 
-                // Since only two possible errors are possible, we know that the error is a 409 conflict.
-                if ($status == 409) {
+                if ($returnValue == 409) {
                     $this->error('A homepage file already exists. Use --force to overwrite.');
                     return 409;
                 }
             }
         }
 
-        $this->postHandleHook();
+        $this->askToRebuildSite();
 
         return 0;
     }
 
-    /**
-     * Prompt for which file to publish.
-     *
-     * @return string selected choice
-     */
-    protected function promptForFile(): string
+    protected function promptForHomepage(): string
     {
         $choice = $this->choice(
             'Which homepage do you want to publish?',
-            choices: $this->publishableChoices(),
-            default: 0
+            $this->formatPublishableChoices(),
+            0
         );
 
-        $choice = $this->parseChoice($choice);
+        $choice = $this->parseChoiceIntoKey($choice);
 
-        $this->line('<info>Selected page </info>[<comment>' . str_replace('homepage-', '', $choice) . '</comment>]');
+        $this->line("<info>Selected page</info> [<comment>$choice</comment>]");
         $this->newLine();
 
         return $choice;
     }
 
-    /**
-     * The choices available via the prompt.
-     *
-     * @return array
-     */
-    protected function publishableChoices(): array
+    protected function formatPublishableChoices(): array
     {
         $keys = [];
         foreach (PublishesHomepageView::$homePages as $key => $value) {
-            $keys[] = '<comment>' . $key . '</comment>: ' . $value['description'];
+            $keys[] = "<comment>$key</comment>: {$value['description']}";
         }
         return $keys;
     }
 
-    /**
-     * Parse the choice into a valid key.
-     *
-     * @param string $choice
-     * @return string
-     */
-    protected function parseChoice(string $choice): string
+    protected function parseChoiceIntoKey(string $choice): string
     {
-        // Strip the <comment> and </comment> tags
-        $choice = str_replace('<comment>', '', $choice);
-        $choice = str_replace('</comment>', '', $choice);
-        // return everything before the colon
-        return strstr($choice, ':', true);
+        return strstr(str_replace(['<comment>', '</comment>'], '', $choice), ':', true);
     }
 
-    /**
-     * @deprecated v0.10.0 will be moved into shared trait.
-     */
-    protected function postHandleHook()
+    /** @deprecated v0.10.0 will be moved into shared trait.  */
+    protected function askToRebuildSite()
     {
         if ($this->option('no-interaction')) {
             return;
@@ -117,11 +96,7 @@ class HydePublishHomepageCommand extends Command
         }
     }
 
-    /**
-     * Allow default files to be overwritten.
-     * @see https://github.com/hydephp/framework/issues/67
-     */
-    protected function canOverwriteFile(): bool
+    protected function canExistingIndexFileBeOverwritten(): bool
     {
         if (!file_exists(Hyde::path('resources/views/pages/index.blade.php')) || $this->option('force')) {
             return true;
