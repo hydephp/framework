2ed6c254327dad5c1cb41fc2d58e1faead3b0a29
Caen De Silva <caen@desilva.se>
2022-04-09 11:56:42 +0200

    Refactor the entire command not use service provider as base

--- src/Commands/HydePublishHomepageCommand.php
+++ src/Commands/HydePublishHomepageCommand.php
@@ -2,91 +2,72 @@
 
 namespace Hyde\Framework\Commands;
 
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Arr;
+use Hyde\Framework\Services\FileCacheService;
+use Hyde\Framework\Actions\PublishesHomepageView;
+use Hyde\Framework\Hyde;
+use LaravelZero\Framework\Commands\Command;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\ServiceProvider;
 
 /**
  * Publish one of the default homepages.
  */
-class HydePublishHomepageCommand extends HydeBasePublishingCommand
+class HydePublishHomepageCommand extends Command
 {
     protected $signature = 'publish:homepage
                                 {homepage? : The name of the page to publish}
                                 {--force : Overwrite any existing files}';
 
-    protected $description = 'Publish one of the default homepages';
+    protected $description = 'Publish one of the default homepages to index.blade.php.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     * @return void
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
+    protected string $selected;
 
-        $this->files = $files;
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     *
-     * @throws \League\Flysystem\FilesystemException
-     */
     public function handle(): int
     {
-        if ($this->argument('homepage') !== null) {
-            $this->tags = [$this->argument('homepage')];
+        $this->selected = $this->argument('homepage') ?? $this->promptForFile();
+
+        $status = (new PublishesHomepageView($this->selected, $this->canOverwriteFile()))->execute();
+
+        if ($status === true) {
+            $this->info("Homepage published successfully!");
         } else {
-            $this->determineWhatShouldBePublished();
+            if (is_numeric($status)) {
+                if ($status == 404) {
+                    $this->error('Homepage ' . $this->selected . ' does not exist.');
+                    return 404;
+                }
+
+                // Since only two possible errors are possible, we know that the error is a 409 conflict.
+                if ($status == 409) {
+                    $this->error('A homepage file already exists. Use --force to overwrite.');
+                    return 409;
+                }
+            }
         }
 
-        foreach ($this->tags ?: [null] as $tag) {
-            $this->publishTag($tag);
-        }
-
-        $this->info('Published selected homepage');
-
-        if (! $this->option('no-interaction')) {
-            $this->postHandleHook();
-        }
+        $this->postHandleHook();
 
         return 0;
     }
 
     /**
-     * Prompt for which tag to publish.
+     * Prompt for which file to publish.
      *
-     * @return void
+     * @return string selected choice
      */
-    protected function promptForProviderOrTag()
+    protected function promptForFile(): string
     {
         $choice = $this->choice(
             'Which homepage do you want to publish?',
             choices: $this->publishableChoices(),
-            default: 2
+            default: 0
         );
 
-        $this->line('<info>Selected page </info>[<comment>'.str_replace('homepage-', '', $choice).'</comment>]');
+        $choice = $this->parseChoice($choice);
+
+        $this->line('<info>Selected page </info>[<comment>' . str_replace('homepage-', '', $choice) . '</comment>]');
         $this->newLine();
 
-        $this->parseChoice($choice);
-    }
-
-    /**
-     * Parse the answer that was given via the prompt.
-     *
-     * @param  string  $choice
-     * @return void
-     */
-    protected function parseChoice(string $choice)
-    {
-        $this->tags = [$choice];
+        return $choice;
     }
 
     /**
@@ -96,27 +77,58 @@ class HydePublishHomepageCommand extends HydeBasePublishingCommand
      */
     protected function publishableChoices(): array
     {
-        return array_merge(
-            [],
-            Arr::sort(
-                array_flip(array_filter(
-                    array_flip(ServiceProvider::publishableGroups()),
-                    fn ($key) => str_starts_with($key, 'homepage-'),
-                    ARRAY_FILTER_USE_KEY
-                ))
-            )
-        );
+        $keys = [];
+        foreach (PublishesHomepageView::$homePages as $key => $value) {
+            $keys[] = '<comment>' . $key . '</comment>: ' . $value['description'];
+        }
+        return $keys;
     }
 
+    /**
+     * Parse the choice into a valid key.
+     *
+     * @param string $choice
+     * @return string
+     */
+    protected function parseChoice(string $choice): string
+    {
+        // Strip the <comment> and </comment> tags
+        $choice = str_replace('<comment>', '', $choice);
+        $choice = str_replace('</comment>', '', $choice);
+        // return everything before the colon
+        return strstr($choice, ':', true);
+    }
+
+    /**
+     * @deprecated v0.10.0 will be moved into shared trait.
+     */
     protected function postHandleHook()
     {
-        $prompt = $this->ask('Would you like to rebuild the site?', 'Yes');
-        if (str_contains(strtolower($prompt), 'y')) {
+        if ($this->option('no-interaction')) {
+            return;
+        }
+
+        if ($this->confirm('Would you like to rebuild the site?', 'Yes')) {
             $this->line('Okay, building site!');
             Artisan::call('build');
             $this->info('Site is built!');
         } else {
             $this->line('Okay, you can always run the build later!');
         }
+    }
+
+    /**
+     * Allow default files to be overwritten.
+     * @see https://github.com/hydephp/framework/issues/67
+     */
+    protected function canOverwriteFile(): bool
+    {
+        if (!file_exists(Hyde::path('resources/views/pages/index.blade.php')) || $this->option('force')) {
+            return true;
+        }
+
+        return FileCacheService::checksumMatchesAny(md5_file(
+            Hyde::path('resources/views/pages/index.blade.php')
+        )) ?? $this->option('force');
     }
 }
