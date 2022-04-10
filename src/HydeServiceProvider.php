97c78ac025be031376c49153498a9bd69be4271c
Caen De Silva <caen@desilva.se>
2022-04-10 18:42:45 +0200

    Remove deprecated command

--- src/HydeServiceProvider.php
+++ src/HydeServiceProvider.php
@@ -37,7 +37,6 @@ class HydeServiceProvider extends ServiceProvider
             Commands\HydePublishFrontendResourcesCommand::class,
             Commands\HydeRebuildStaticSiteCommand::class,
             Commands\HydeBuildStaticSiteCommand::class,
-            Commands\HydePublishTestFilesCommand::class,
             Commands\HydeMakePostCommand::class,
             Commands\HydeMakePageCommand::class,
             Commands\HydeValidateCommand::class,
