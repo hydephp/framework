411fb40e16d73380038d06df03a71d4f2d2063ff
Caen De Silva <caen@desilva.se>
2022-04-10 15:19:17 +0200

    Update and simplify the command, rename signature

--- src/Commands/HydePublishConfigsCommand.php
+++ src/Commands/HydePublishConfigsCommand.php
@@ -2,82 +2,26 @@
 
 namespace Hyde\Framework\Commands;
 
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\ServiceProvider;
+use LaravelZero\Framework\Commands\Command;
+use Illuminate\Support\Facades\File;
+use Hyde\Framework\Hyde;
 
 /**
  * Publish the Hyde Config Files.
  *
  * @uses HydeBasePublishingCommand
  */
-class HydePublishConfigsCommand extends HydeBasePublishingCommand
+class HydePublishConfigsCommand extends Command
 {
-    protected $signature = 'publish:configs {--force : Overwrite any existing files}';
+    protected $signature = 'update:configs';
+    protected $description = 'Publish the default configuration files';
 
-    protected $description = 'Publish the Hyde configuration files';
-
-    /**
-     * Create a new command instance.
-     *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     * @return void
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
-
-        $this->files = $files;
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     *
-     * @throws \League\Flysystem\FilesystemException
-     */
     public function handle(): int
     {
-        $this->tags = array_flip(array_filter(
-            array_flip(ServiceProvider::publishableGroups()),
-            fn ($key) => str_starts_with($key, 'configs'),
-            ARRAY_FILTER_USE_KEY
-        ));
+        File::copyDirectory(Hyde::vendorPath('config'), Hyde::path('config'));
 
-        foreach ($this->tags ?: [null] as $tag) {
-            $this->publishTag($tag);
-        }
-
-        $this->info('Publishing complete.');
+        $this->line('<info>Published config files to</info> <comment>' . Hyde::path('config') . '</comment>');
 
         return 0;
-    }
-
-    /**
-     * Determine the provider or tag(s) to publish.
-     *
-     * @return void
-     */
-    protected function determineWhatShouldBePublished()
-    {
-    }
-
-    /**
-     * Prompt for which tag to publish.
-     *
-     * @return void
-     */
-    protected function promptForProviderOrTag()
-    {
-    }
-
-    /**
-     * The choices available via the prompt.
-     *
-     * @return array
-     */
-    protected function publishableChoices(): array
-    {
-        return [];
     }
 }
