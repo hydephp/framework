b3962a9aaf07d621e97c7140172240c1f643249e
Caen De Silva <caen@desilva.se>
2022-04-11 16:04:25 +0200

    Add the new traits to offload logic

--- src/Models/MarkdownPost.php
+++ src/Models/MarkdownPost.php
@@ -7,11 +7,18 @@ use Hyde\Framework\MarkdownPostParser;
 class MarkdownPost extends MarkdownDocument
 {
     use HasMetadata;
+    use HasDateString;
 
     public ?Image $image = null;
-    public ?Metadata $metadata = null;
-    public ?Datestring $date = null;
 
     public static string $sourceDirectory = '_posts';
     public static string $parserClass = MarkdownPostParser::class;
+
+    public function __construct(array $matter, string $body, string $title = '', string $slug = '')
+    {
+        parent::__construct($matter, $body, $title, $slug);
+
+        $this->constructMetadata();
+        $this->constructDateString();
+    }
 }
