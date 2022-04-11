17ff6dbb00f585138a6c1bf616b9c3a1555de502
Caen De Silva <caen@desilva.se>
2022-04-11 20:15:39 +0200

    Fix the failing tests, caused by a case typo

--- src/Models/HasDateString.php
+++ src/Models/HasDateString.php
@@ -4,12 +4,12 @@ namespace Hyde\Framework\Models;
 
 trait HasDateString
 {
-    public ?Datestring $date = null;
+    public ?DateString $date = null;
 
     public function constructDateString(): void
     {
         if (isset($this->matter['date'])) {
-            $this->date = new Datestring($this->matter['date']);
+            $this->date = new DateString($this->matter['date']);
         }
     }
 }
