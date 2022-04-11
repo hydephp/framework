7cab5db5e0c050d3eef456dd8add4db29790f067
Caen De Silva <caen@desilva.se>
2022-04-11 14:33:03 +0200

    Indent and reorder meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,23 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:site_name" content="{{ config('hyde.name', 'HydePHP') }}">
-
-    @if (file_exists(Hyde::path('_media/favicon.ico')))
-        <link rel="shortcut icon" href="{{ Hyde::relativePath('media/favicon.ico', $currentPage) }}" type="image/x-icon">
-    @endif
-
-    @stack('meta')
-    
-    @include('hyde::layouts.meta') 
-
     <title>{{ isset($title) ? config('hyde.name', 'HydePHP') . ' - ' . $title : config('hyde.name', 'HydePHP') }}</title>
 
+    @if (file_exists(Hyde::path('_media/favicon.ico'))) 
+    <link rel="shortcut icon" href="{{ Hyde::relativePath('media/favicon.ico', $currentPage) }}" type="image/x-icon">
+    @endif
+
+    <!-- Config Defined Tags -->
+    @foreach (config('hyde.meta', []) as $name => $content) 
+    <meta name="{{ $name }}" content="{{ $content }}">
+    @endforeach
+
+    @stack('meta')
+  
     <!-- The compiled Tailwind styles -->
     <link rel="stylesheet" href="{{ Hyde::relativePath('media/app.css', $currentPage) }}">
     
     <!-- The core Hyde stylesheet -->
     <link rel="stylesheet" href="{{ Hyde::relativePath('media/hyde.css', $currentPage) }}">
-
+  
+    <!-- Include any extra tags to include in the <head> section -->
+    @include('hyde::layouts.meta') 
 </head>
 <body id="app" class="flex flex-col min-h-screen overflow-x-hidden">
     @includeUnless($withoutNavigation ?? false, 'hyde::layouts.navigation') 
--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,6 +1,1 @@
-<!-- Include any extra tags to include in the <head> section -->
-
-<!-- Config Defined Tags -->
-@foreach (config('hyde.meta', []) as $name => $content)
-<meta name="{{ $name }}" content="{{ $content }}">
-@endforeach
+{{-- You can place any extra meta tags here. They will be placed before the closing </head> tag. --}}
