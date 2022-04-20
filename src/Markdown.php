c6982112a6cfd2ef31b9ddcb7b6e5b45411486c1
Caen De Silva <caen@desilva.se>
2022-04-20 18:52:02 +0200

    Remove @method doc

--- src/Markdown.php
+++ src/Markdown.php
@@ -8,8 +8,6 @@ use Hyde\Framework\Actions\ServiceActions\HasMarkdownFeatures;
  * General interface for Markdown services.
  *
  * @see \Hyde\Framework\Services\MarkdownConverterService
- *
- * @method static hasTableOfContents()
  */
 class Markdown
 {
