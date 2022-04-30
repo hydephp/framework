aede0b3837110c26d2af27aa4bbbf8176e4d7ab6
Caen De Silva <caen@desilva.se>
2022-04-30 13:41:46 +0200

    Fix namespace

--- tests/Feature/Services/HasConfigurableMarkdownFeaturesTest.php
+++ tests/Feature/Services/HasConfigurableMarkdownFeaturesTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Services;
+namespace  Tests\Feature\Services;
 
 use Hyde\Framework\Concerns\Markdown\HasConfigurableMarkdownFeatures;
 use Hyde\Framework\Models\DocumentationPage;
