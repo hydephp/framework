5f63d740b5d44162ffdbc8c9f342c9c0522db445
Caen De Silva <caen@desilva.se>
2022-05-01 18:43:46 +0200

    Remove unnecessary PHPDocs

--- src/Features.php
+++ src/Features.php
@@ -12,7 +12,7 @@ namespace Hyde\Framework;
 class Features
 {
     /**
-     * Determine if the given feature is enabled.
+     * Determine if the given specified is enabled.
      *
      * @param  string  $feature
      * @return bool
@@ -21,66 +21,42 @@ class Features
     {
         return in_array($feature, config('hyde.features', []));
     }
+ 
 
     /**
-     * Determine if the site has blog posts enabled.
-     *
-     * @return bool
+     * ================================================
+     * Determine if a given feature is enabled.
+     * ================================================
      */
+
     public static function hasBlogPosts(): bool
     {
         return static::enabled(static::blogPosts());
     }
 
-    /**
-     * Determine if the site has custom Blade pages enabled.
-     *
-     * @return bool
-     */
     public static function hasBladePages(): bool
     {
         return static::enabled(static::bladePages());
     }
 
-    /**
-     * Determine if the site has custom Markdown pages enabled.
-     *
-     * @return bool
-     */
     public static function hasMarkdownPages(): bool
     {
         return static::enabled(static::markdownPages());
     }
 
-    /**
-     * Determine if the site has Laradocgen enabled.
-     *
-     * @return bool
-     */
     public static function hasDocumentationPages(): bool
     {
         return static::enabled(static::documentationPages());
     }
 
-    /**
-     * Determine if the site has Dark Mode enabled.
-     *
-     * @return bool
-     */
     public static function hasDarkmode(): bool
     {
         return static::enabled(static::darkmode());
     }
 
     /**
-     * Determine if the site has Torchlight enabled.
-     *
-     * Torchlight is an API for Syntax Highlighting. By default, it is enabled
-     * automatically when an API token is set in the .env file.
-     *
-     * It is disabled when running tests.
-     *
-     * @return bool
+     * Torchlight is by default enabled automatically when an API token
+     * is set in the .env file but is disabled when running tests.
      */
     public static function hasTorchlight(): bool
     {
@@ -89,64 +65,40 @@ class Features
             && (app('env') !== 'testing');
     }
 
+    
     /**
-     * Enable the blog post feature.
-     *
-     * @return string
+     * ================================================
+     * Enable a given feature to be used in the config.
+     * ================================================
      */
+    
     public static function blogPosts(): string
     {
         return 'blog-posts';
     }
 
-    /**
-     * Enable the Blade page feature.
-     *
-     * @return string
-     */
     public static function bladePages(): string
     {
         return 'blade-pages';
     }
 
-    /**
-     * Enable the Markdown page feature.
-     *
-     * @return string
-     */
     public static function markdownPages(): string
     {
         return 'markdown-pages';
     }
 
-    /**
-     * Enable the documentation page feature.
-     *
-     * @return string
-     */
     public static function documentationPages(): string
     {
         return 'documentation-pages';
     }
 
-    /**
-     * Enable the Darkmode feature.
-     *
-     * @return string
-     */
     public static function darkmode(): string
     {
         return 'darkmode';
     }
 
-    /**
-     * Enable the Torchlight integration.
-     *
-     * @return string
-     */
     public static function torchlight(): string
     {
         return 'torchlight';
     }
-    
 }
