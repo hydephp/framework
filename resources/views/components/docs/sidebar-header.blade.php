7c50b4f3a172315008344a0f40db7b7516b6215c
Caen De Silva <caen@desilva.se>
2022-05-02 17:18:05 +0200

    Allow documentation sidebar header name to be changed

--- resources/views/components/docs/sidebar-header.blade.php
+++ resources/views/components/docs/sidebar-header.blade.php
@@ -2,10 +2,11 @@
     <h2 class="font-bold text-gray-700 hover:text-gray-900 dark:text-gray-200 w-fit">
         @if(Hyde::docsIndexPath() !== false)
         <a href="{{ basename(Hyde::docsIndexPath()) }}">
-            {{ config('hyde.name') }} Docs
+            {{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+
         </a>
         @else
-            {{ config('hyde.name') }} Docs
+            {{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
         @endif
     </h2>
     <div class="ml-auto">
