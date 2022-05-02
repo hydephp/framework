17943a75f180fc5d17822bf4bee3339db356acb7
Caen De Silva <caen@desilva.se>
2022-05-02 16:50:52 +0200

    Remove Hyde::path() and check if files exist

--- tests/Unit/EnsureCommandsFollowNamingConventionTest.php
+++ tests/Unit/EnsureCommandsFollowNamingConventionTest.php
@@ -2,14 +2,19 @@
 
 namespace Tests\Unit;
 
-use Hyde\Framework\Hyde;
 use Tests\TestCase;
 
 class EnsureCommandsFollowNamingConventionTest extends TestCase
 {
     public function test_ensure_commands_follow_naming_convention()
     {
-        foreach (glob(Hyde::path('vendor/hyde/framework/src/commands/*.php')) as $filepath) {
+        $files = glob('vendor/hyde/framework/src/commands/*.php');
+
+        if (empty($files)) {
+            $this->markTestSkipped('No commands found.');
+        }
+
+        foreach ($files as $filepath) {
             $filename = basename($filepath, '.php');
             $this->assertStringStartsWith('Hyde', $filename);
             $this->assertStringEndsWith('Command', $filename);
