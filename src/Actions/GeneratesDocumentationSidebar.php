8a5f9d76433a8f51735495d52ab81d93806177c9
Caen De Silva <caen@desilva.se>
2022-05-02 15:45:09 +0200

    Fix  #232

--- src/Actions/GeneratesDocumentationSidebar.php
+++ src/Actions/GeneratesDocumentationSidebar.php
@@ -30,7 +30,7 @@ class GeneratesDocumentationSidebar
 
         foreach (CollectionService::getSourceFileListForModel(DocumentationPage::class) as $slug) {
             if ($slug !== 'index') {
-                $order = array_search($slug, $orderArray) ?: 999;
+                $order = ($position = array_search($slug, $orderArray)) === null ? 999 : $position;
 
                 $array[] = [
                     'slug' => $slug,
