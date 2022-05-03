ee4a64d9a22314339b002bbd856b2f79c08bffea
Caen De Silva <caen@desilva.se>
2022-05-03 16:23:43 +0200

    Add the test

---
+++ tests/Feature/ConfigurableFeaturesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Hyde\Framework\Features;
+use Illuminate\Support\Facades\Config;
+use Tests\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features
+ */
+class ConfigurableFeaturesTest extends TestCase
+{
+    // Test HasFeature methods return false when feature is not enabled
+    public function testHasFeatureReturnsFalseWhenFeatureIsNotEnabled()
+    {
+        Config::set('hyde.features', []);
+        // Foreach method in Features class that begins with "has"
+        foreach (get_class_methods(Features::class) as $method) {
+            if (str_starts_with($method, 'has')) {
+                // Call method and assert false
+                $this->assertFalse(Features::$method());
+            }
+        }
+    }
+
+    // Test HasFeature methods return true when feature is enabled
+    public function testHasFeatureReturnsTrueWhenFeatureIsEnabled()
+    {
+        $features = [];
+        foreach (get_class_methods(Features::class) as $method) {
+            if (!str_starts_with($method, 'has') && $method !== 'enabled') {
+                $features[] = '\Hyde\Framework\Features::' . $method . '()';
+            }
+        }
+
+        Config::set('hyde.features', $features);
+
+        foreach ($features as $feature) {
+            $this->assertTrue(Features::enabled($feature));
+        }
+    }
+}
