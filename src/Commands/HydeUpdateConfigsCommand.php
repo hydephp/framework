e4f6f01941dc0a1d51a415779fdf03f0073942dc
Caen De Silva <caen@desilva.se>
2022-05-03 18:37:49 +0200

    Change the command description to warn about outdated configs

--- src/Commands/HydeUpdateConfigsCommand.php
+++ src/Commands/HydeUpdateConfigsCommand.php
@@ -2,6 +2,7 @@
 
 namespace Hyde\Framework\Commands;
 
+use Hyde\Framework\Actions\ChecksIfConfigIsUpToDate;
 use Hyde\Framework\Hyde;
 use Illuminate\Support\Facades\File;
 use LaravelZero\Framework\Commands\Command;
@@ -16,6 +17,18 @@ class HydeUpdateConfigsCommand extends Command
     protected $signature = 'update:configs';
     protected $description = 'Publish the default configuration files';
 
+    public function __construct()
+    {
+        parent::__construct();
+    
+        if ($this->checkIfConfigIsOutOfDate() && config('hyde.warnAboutOutdatedConfig', true)) {
+            $this->setDescription(
+                '<comment>⚠ Your configuration may be out of date. </comment>' .
+                'Run this command to update them.'
+            );
+        }
+    }
+
     public function handle(): int
     {
         File::copyDirectory(Hyde::vendorPath('config'), Hyde::path('config'));
@@ -23,5 +36,10 @@ class HydeUpdateConfigsCommand extends Command
         $this->line('<info>Published config files to</info> <comment>'.Hyde::path('config').'</comment>');
 
         return 0;
+    }
+
+    protected function checkIfConfigIsOutOfDate(): bool
+    {
+        return ! (new ChecksIfConfigIsUpToDate)->execute();
     }
 }
