3093ebf65556e185649a40fd8459caa3fa250d7d
Caen De Silva <caen@desilva.se>
2022-05-05 18:21:29 +0200

    Move all head tags into blade component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('hyde.language', 'en') }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:site_name" content="{{ config('hyde.name', 'HydePHP') }}">
-    <title>{{ isset($title) ? config('hyde.name', 'HydePHP') . ' - ' . $title : config('hyde.name', 'HydePHP') }}</title>
-
-    @if (file_exists(Hyde::path('_media/favicon.ico'))) 
-    <link rel="shortcut icon" href="{{ Hyde::relativePath('media/favicon.ico', $currentPage) }}" type="image/x-icon">
-    @endif
-
-    {{-- Config Defined Tags --}}
-    @foreach (config('hyde.meta', []) as $name => $content) 
-    <meta name="{{ $name }}" content="{{ $content }}">
-    @endforeach
-
-    @stack('meta')
-
-    {{-- App Stylesheets --}}
-    @include('hyde::layouts.styles')
-  
-    {{-- Include any extra tags to include in the <head> section --}}
-    @include('hyde::layouts.meta') 
-
-    @if(Hyde::features('darkmode'))
-    {{-- Check the local storage for theme preference to avoid FOUC --}}
-    <script>if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) { document.documentElement.classList.add('dark'); } else { document.documentElement.classList.remove('dark') } </script>
-    @endif
+    @include('hyde::layouts.head')
 </head>
 <body id="app" class="flex flex-col min-h-screen overflow-x-hidden dark:bg-gray-900 dark:text-white">
     <a href="#content" id="skip-to-content">Skip to content</a>
@@ -38,10 +13,6 @@
 
     @includeUnless(config('hyde.footer.enabled', true) && ($withoutNavigation ?? false), 'hyde::layouts.footer') 
 
-    {{-- App Scripts --}}
     @include('hyde::layouts.scripts') 
-
-    {{-- Include any extra scripts to include before the closing <body> tag --}}
-    @stack('scripts')
 </body>
 </html>
