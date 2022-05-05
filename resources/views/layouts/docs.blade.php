6ca230964211c79fe19df5954a65ad846500ba5e
Caen De Silva <caen@desilva.se>
2022-05-05 15:36:18 +0200

    Port lagrafo (wip)

--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -1,21 +1,87 @@
-{{-- The Documentation Page Layout is based on Laradocgen --}}
-@extends('hyde::layouts.app')
-@section('content')
-@php($withoutNavigation = true)
+<!doctype html>
+<html lang="en">
 
-<nav id="documentation-navigation" class="fixed top-0 w-screen md:w-fit right-0 h-16 p-4 shadow-lg sm:shadow-xl overflow-hidden bg-white dark:bg-gray-800 md:bg-transparent dark:md:bg-transparent md:shadow-none z-30">
-	@include('hyde::components.docs.navigation')
-</nav>
-<aside id="documentation-sidebar" class="w-64 h-screen hidden md:flex flex-col fixed top-0 left-0 shadow-md overflow-hidden bg-white dark:bg-gray-800 z-20">
-	@include('hyde::components.docs.sidebar')
-</aside>
-<main id="documentation-content" class="mx-auto max-w-7xl py-16 px-8 mt-8 md:mt-0 md:absolute md:left-72 xl:left-80">
-	<a name="content" id="content"></a>
-	@include('hyde::components.docs.content')
-</main>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ isset($title) ? config('hyde.name', 'HydePHP') . ' - ' . $title : config('hyde.name', 'HydePHP') }}
+	</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/caendesilva/lagrafo@v0.1.0-beta/dist/lagrafo.min.css">
+</head>
 
-<div id="sidebar-backdrop" title="Click to close sidebar" onClick="hideSidebar()" style="display: none;"></div>
-@if(config('hyde.documentationPageTableOfContents.smoothPageScrolling', true))
-<style> html { scroll-behavior: smooth; } </style>
-@endif
-@endsection
+<body id="lagrafo-app">
+	<script>
+		document.body.classList.add('js-enabled');
+	</script>
+
+	<nav id="mobile-navigation">
+		<strong>
+			@if(Hyde::docsIndexPath() !== false)
+			<a href="{{ basename(Hyde::docsIndexPath()) }}">
+				{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+			</a>
+			@else
+			{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+			@endif
+		</strong>
+		<button id="sidebar-toggle" title="Toggle sidebar" aria-label="Toggle sidebar navigation menu">
+			<span class="icon-bar" role="presentation"></span>
+			<span class="icon-bar" role="presentation"></span>
+			<span class="icon-bar" role="presentation"></span>
+			<span class="icon-bar" role="presentation"></span>
+		</button>
+	</nav>
+	<aside id="sidebar">
+		<header id="sidebar-header">
+			<div id="sidebar-brand">
+				<strong>
+					@if(Hyde::docsIndexPath() !== false)
+					<a href="{{ basename(Hyde::docsIndexPath()) }}">
+						{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+
+					</a>
+					@else
+					{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+					@endif
+				</strong>
+			</div>
+		</header>
+		<nav id="sidebar-navigation">
+			<ul id="sidebar-navigation-menu" role="list">
+				@foreach (Hyde\Framework\Actions\GeneratesDocumentationSidebar::get($currentPage) as $item)
+				<li @class([ 'sidebar-navigation-item' , 'active'=> $item['active']
+					])>
+					@if($item['active'])
+					<a href="{{ $item['slug'] }}.html" aria-current="true" class="sidebar-navigation-item">{{
+						$item['title'] }}</a>
+
+					@if(isset($docs->tableOfContents))
+					{!! ($docs->tableOfContents) !!}
+					@endif
+					@else
+					<a href="{{ $item['slug'] }}.html" class="sidebar-navigation-item">{{ $item['title'] }}</a>
+					@endif
+				</li>
+				@endforeach
+			</ul>
+		</nav>
+		<footer id="sidebar-footer">
+			<p>
+				<a href="{{ Hyde::relativePath('index.html', $currentPage) }}">Back to home page</a>
+			</p>
+		</footer>
+	</aside>
+	<main id="content">
+		<article id="document" itemscope itemtype="https://schema.org/Article" @class(['mx-auto prose dark:prose-invert
+			max-w-3xl', 'torchlight-enabled'=> Hyde\Framework\Features::hasTorchlight()])>
+			<section id="document-main-content" itemprop="articleBody">
+				{!! $markdown !!}
+			</section>
+		</article>
+	</main>
+	<script defer="" src="https://cdn.jsdelivr.net/gh/caendesilva/lagrafo@v0.1.0-beta/dist/lagrafo.min.js"></script>
+</body>
+
+</html>
