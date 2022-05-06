e6b4d062dfd8e19f1c23bb6901bc5cadf25da62f
Caen De Silva <caen@desilva.se>
2022-05-06 20:09:14 +0200

    Move all meta tags to the layout component

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,24 +1,16 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="og:site_name" content="{{ config('hyde.name', 'HydePHP') }}">
 <title>{{ isset($title) ? config('hyde.name', 'HydePHP') . ' - ' . $title : config('hyde.name', 'HydePHP') }}</title>
 
 @if (file_exists(Hyde::path('_media/favicon.ico'))) 
 <link rel="shortcut icon" href="{{ Hyde::relativePath('media/favicon.ico', $currentPage) }}" type="image/x-icon">
 @endif
 
-{{-- Config Defined Tags --}}
-@foreach (config('hyde.meta', []) as $name => $content) 
-<meta name="{{ $name }}" content="{{ $content }}">
-@endforeach
-
-@stack('meta')
+{{-- App Meta Tags --}}
+@include('hyde::layouts.meta') 
 
 {{-- App Stylesheets --}}
 @include('hyde::layouts.styles')
-
-{{-- Include any extra tags to include in the <head> section --}}
-@include('hyde::layouts.meta') 
 
 @if(Hyde::features('darkmode'))
 {{-- Check the local storage for theme preference to avoid FOUC --}}
--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,1 +1,10 @@
-{{-- You can place any extra meta tags here. They will be placed before the closing </head> tag. --}}
+{{-- Config Defined Tags --}}
+@foreach (config('hyde.meta', []) as $name => $content) 
+<meta name="{{ $name }}" content="{{ $content }}">
+@endforeach
+
+{{-- Add any extra tags to include in the <head> section --}}
+<meta property="og:site_name" content="{{ config('hyde.name', 'HydePHP') }}">
+
+{{-- Add any extra meta tags to include after the others --}}
+@stack('meta')
