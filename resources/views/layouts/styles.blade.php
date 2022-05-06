b080e1c7ce784f045c5749e9d9e5ba94a8cecb77
Caen De Silva <caen@desilva.se>
2022-05-06 20:10:01 +0200

    Add a styles stack

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -7,3 +7,6 @@
 @if(Hyde::assetManager()->hasMediaFile('app.css'))
 <link rel="stylesheet" href="{{ Hyde::relativeLink('media/app.css', $currentPage) }}">
 @endif
+
+{{-- Add any extra styles to include after the others --}}
+@stack('styles')
