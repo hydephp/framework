10a393b5a45444ee2ad79a2a3f8024b3d0cc3e0e
Caen De Silva <caen@desilva.se>
2022-05-07 14:14:55 +0200

    Remove old documentation layouts

--- resources/views/components/docs/navigation.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="flex flex-row items-center justify-between h-full">
-	<div class="md:hidden">
-	@include('hyde::components.docs.sidebar-header')
-	</div>
-	<div class="ml-auto">
-        @include('hyde::components.navigation.theme-toggle-button')
-    </div>
-	@include('hyde::components.docs.sidebar-toggle')
-</div>
--- resources/views/components/docs/sidebar-footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="p-4 mt-auto border-t dark:border-gray-700">
-    <a href="{{ Hyde::relativePath('index.html', $currentPage) }}" class="opacity-75 hover:opacity-100">Back to home page</a>
-</footer>
