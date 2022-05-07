fbcae7cacd100267440b362a97f97d7bbdee09a9
Caen De Silva <caen@desilva.se>
2022-05-07 11:50:00 +0200

    Create the sidebar models

---
+++ src/Models/DocumentationSidebar.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Hyde\Framework\Models;
+
+use Hyde\Framework\Contracts\DocumentationSidebarContract;
+use Illuminate\Support\Collection;
+
+/**
+ * The documentation sidebar, containing all the sidebar items.
+ * 
+ * Extends the Illuminate\Support\Collection class and has helper
+ * methods to fluently add DocumentationSidebarItems to the 
+ * collection using method chaining.
+ * 
+ * @see \Tests\Feature\Services\DocumentationSidebarServiceTest
+ */
+class DocumentationSidebar extends Collection implements DocumentationSidebarContract
+{
+    public function addItem(DocumentationSidebarItem $item): self
+    {
+        $this->push($item);
+
+        return $this;
+    }
+
+    public function sortItems(): self
+    {
+        return $this->sortBy('priority')
+            ->values(); // Reset the keys to consecutively numbered indexes:
+    }
+
+    public function getCollection(): self
+    {
+        return $this;
+    }
+}
