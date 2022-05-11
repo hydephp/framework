98aa54ebf10c0f8d3732676e1c9d023bee19190a
Caen De Silva <caen@desilva.se>
2022-05-11 14:10:37 +0200

    Extract sidebar to component

--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -45,23 +45,7 @@
 			</div>
 		</header>
 		<nav id="sidebar-navigation">
-			<ul id="sidebar-navigation-menu" role="list">
-				@foreach (Hyde\Framework\Services\DocumentationSidebarService::get() as $item)
-				<li @class([ 'sidebar-navigation-item' , 'active'=> $item->destination === basename($currentPage)])>
-					@if($item->destination === basename($currentPage))
-					<a href="{{ $item->destination }}.html" aria-current="true">{{
-						$item->label }}</a>
-
-					@if(isset($docs->tableOfContents))
-					<span class="sr-only">Table of contents</span>
-					{!! ($docs->tableOfContents) !!}
-					@endif
-					@else
-					<a href="{{ $item->destination }}.html">{{ $item->label }}</a>
-					@endif
-				</li>
-				@endforeach
-			</ul>
+			@include('hyde::components.docs.sidebar-navigation-menu')
 		</nav>
 		<footer id="sidebar-footer">
 			<p>
