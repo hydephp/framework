580cd9a616ea5ea7303ac92a04f04a6903828d15
Caen De Silva <caen@desilva.se>
2022-05-14 22:30:55 +0200

    Add autodiscovery information

--- src/Services/BuildService.php
+++ src/Services/BuildService.php
@@ -42,6 +42,8 @@ class BuildService
 
     /**
      * Get the source directory path of a model.
+     * 
+     * This is what powers the Hyde autodiscovery.
      */
     public static function getFilePathForModelClassFiles(string $model): string
     {
