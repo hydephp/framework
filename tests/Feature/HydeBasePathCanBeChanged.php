cec4dba553d3f6b55c222c94200c3935de1f9401
Caen De Silva <caen@desilva.se>
2022-05-15 15:22:26 +0200

    Create the test

--- tests/Feature/HydeBasePathCanBeChanged.php
+++ tests/Feature/HydeBasePathCanBeChanged.php
@@ -2,15 +2,22 @@
 
 namespace Tests\Feature;
 
+use Hyde\Framework\Hyde;
 use Tests\TestCase;
 
 /**
  * Class HydeBasePathCanBeChanged.
+ *
+ * @covers \Hyde\Framework\Hyde::getBasePath()
+ * @covers \Hyde\Framework\Hyde::setBasePath()
+ * @covers \Hyde\Framework\Hyde::path()
  */
 class HydeBasePathCanBeChanged extends TestCase
 {
-    public function test_example()
+    public function test_hyde_base_path_can_be_changed()
     {
-        // TODO: Implement class
+        Hyde::setBasePath('/foo/bar');
+        $this->assertEquals('/foo/bar', Hyde::getBasePath());
+        $this->assertEquals('/foo/bar', Hyde::path());
     }
 }
