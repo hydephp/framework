ee2841dc7d76728463e82f4abafd84f39a773766
Caen De Silva <caen@desilva.se>
2022-05-15 18:32:50 +0200

    Change assertion naming to match related test

--- tests/Unit/FileHelperRelativeLinkTest.php
+++ tests/Unit/FileHelperRelativeLinkTest.php
@@ -21,38 +21,38 @@ class FileHelperRelativeLinkTest extends TestCase
     // Test helper injects the proper number of `../` 
     public function test_helper_injects_proper_number_of_doubles_slash()
     {
-        $this->assertEquals('../index.html', Hyde::relativeLink('index.html', 'foo/bar.html'));
+        $this->assertEquals('../foo.html', Hyde::relativeLink('foo.html', 'foo/bar.html'));
     }
 
     // Test helper injects the proper number of `../` for deeply nested $current paths
     public function test_helper_injects_proper_number_of_doubles_slash_for_deeply_nested_paths()
     {
-        $this->assertEquals('../../../index.html', Hyde::relativeLink('index.html', 'foo/bar/baz/qux.html'));
+        $this->assertEquals('../../../foo.html', Hyde::relativeLink('foo.html', 'foo/bar/baz/qux.html'));
     }
 
     // Test helper handles destination without file extension
     public function test_helper_handles_destination_without_file_extension()
     {
-        $this->assertEquals('../index', Hyde::relativeLink('index', 'foo/bar.html'));
+        $this->assertEquals('../foo', Hyde::relativeLink('foo', 'foo/bar.html'));
     }
 
     // Test helper handles $current without file extension
     public function test_helper_handles_current_without_file_extension()
     {
-        $this->assertEquals('../index.html', Hyde::relativeLink('index.html', 'foo/bar'));
+        $this->assertEquals('../foo.html', Hyde::relativeLink('foo.html', 'foo/bar'));
     }
 
     // Test helper handles case without any file extensions
     public function test_helper_handles_case_without_any_file_extensions()
     {
-        $this->assertEquals('../index', Hyde::relativeLink('index', 'foo/bar'));
+        $this->assertEquals('../foo', Hyde::relativeLink('foo', 'foo/bar'));
     }
 
     // Test helper handles case with mixed file extensions
     public function test_helper_handles_case_with_mixed_file_extensions()
     {
-        $this->assertEquals('../index.md', Hyde::relativeLink('index.md', 'foo/bar.md'));
-        $this->assertEquals('../index.txt', Hyde::relativeLink('index.txt', 'foo/bar.txt'));
+        $this->assertEquals('../foo.md', Hyde::relativeLink('foo.md', 'foo/bar.md'));
+        $this->assertEquals('../foo.txt', Hyde::relativeLink('foo.txt', 'foo/bar.txt'));
     }
 
     // Test helper handles different file extensions
