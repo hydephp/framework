8dbffcfc9e34a5514005a28f93c9758395eb10f1
Caen De Silva <caen@desilva.se>
2022-05-18 16:31:18 +0200

    Create the test class

---
+++ tests/Feature/Services/SitemapServiceTest/SitemapServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\Services\SitemapServiceTest;
+
+use Hyde\Framework\Services\SitemapService;
+use Tests\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Services\SitemapService
+ */
+class SitemapServiceTest extends TestCase
+{
+
+}
