1f669282d727042df5074f0182bf5e0563d07a91
Caen De Silva <caen@desilva.se>
2022-05-18 16:26:54 +0200

    Create basic sitemap generator

---
+++ src/Services/SitemapService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Hyde\Framework\Services;
+
+use Hyde\Framework\Helpers\Features;
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\BladePage;
+use SimpleXMLElement;
+
+/**
+ * @see \Tests\Feature\Services\SitemapServiceTest
+ */
+class SitemapService
+{
+    public SimpleXMLElement $xmlElement;
+
+    public function __construct()
+    {
+        $this->time_start = microtime(true);
+
+        $this->xmlElement = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+        $this->xmlElement->addAttribute('generator', 'HydePHP '.Hyde::version());
+    }
+
+    public function generate(): self
+    {
+        if (Features::hasBladePages()) {
+            $this->addToSitemap(BladePage::class);
+        }
+
+        return $this;
+    }
+
+    public function addToSitemap(string $model): self
+    {
+        $collection = CollectionService::getSourceFileListForModel($model);
+       
+        foreach ($collection as $page) {
+            $this->xmlElement->addChild('url')->addChild('loc', Hyde::uriPath(Hyde::pageLink($page . '.html')));
+        }
+
+        return $this;
+    }
+
+    public function getXML(): string
+    {
+        $this->xmlElement->addAttribute('processing_time_ms', (string) round((microtime(true) - $this->time_start) * 1000, 2));
+
+        return $this->xmlElement->asXML();
+    }
+
+    public static function generateSitemap(): string
+    {
+        return (new static)->generate()->getXML();
+    }
+
+    public static function canGenerateSitemap(): bool
+    {
+        return (Hyde::uriPath() !== false);
+    }
+}
