b719b0fc1f08620aec7b6e096f6036feb0026846
Caen De Silva <caen@desilva.se>
2022-05-18 22:59:42 +0200

    Formatting

--- src/Services/RssFeedService.php
+++ src/Services/RssFeedService.php
@@ -17,7 +17,8 @@ class RssFeedService
 
     public function __construct()
     {
-        $this->feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
+        $this->feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>
+            <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
         $this->feed->addChild('channel');
 
         $this->addInitialChannelItems();
@@ -73,8 +74,6 @@ class RssFeedService
         $atomLink->addAttribute('href', $this->getLink() . '/rss.xml');
         $atomLink->addAttribute('rel', 'self');
         $atomLink->addAttribute('type', 'application/rss+xml');
-
-
 
         $this->addAdditionalChannelData();
     }
