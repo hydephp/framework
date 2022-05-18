024ce28d1415356e05ad66bf69efa5c56b70966f
Caen De Silva <caen@desilva.se>
2022-05-18 22:56:12 +0200

    Add XML namespace to root element

--- src/Services/RssFeedService.php
+++ src/Services/RssFeedService.php
@@ -17,7 +17,7 @@ class RssFeedService
 
     public function __construct()
     {
-        $this->feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" />');
+        $this->feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
         $this->feed->addChild('channel');
 
         $this->addInitialChannelItems();
