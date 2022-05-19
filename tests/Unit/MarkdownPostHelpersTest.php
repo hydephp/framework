2a3b90bbf2ffab9709a49447b9a4aa80cd14ca9e
Caen De Silva <caen@desilva.se>
2022-05-19 13:07:12 +0200

    Add unit test for fluent Markdown post helpers

---
+++ tests/Unit/MarkdownPostHelpersTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use Hyde\Framework\Models\MarkdownPost;
+use Tests\TestCase;
+
+/**
+ * @see \Hyde\Framework\Models\MarkdownPost
+ */
+class MarkdownPostHelpersTest extends TestCase
+{
+	public function test_get_current_page_path_returns_local_uri_path_for_post_slug()
+	{
+		$post = new MarkdownPost([], '', '', 'foo-bar');
+		$this->assertEquals('posts/foo-bar', $post->getCurrentPagePath());
+	}
+
+	public function test_get_canonical_link_returns_canonical_uri_path_for_post_slug()
+	{
+		config(['hyde.site_url' => 'https://example.com']);
+		$post = new MarkdownPost([], '', '', 'foo-bar');
+		$this->assertEquals('https://example.com/posts/foo-bar.html', $post->getCanonicalLink());
+	}
+
+	public function test_get_canonical_link_returns_pretty_url_when_enabled()
+	{
+		config(['hyde.site_url' => 'https://example.com', 'hyde.prettyUrls' => true]);
+		$post = new MarkdownPost([], '', '', 'foo-bar');
+		$this->assertEquals('https://example.com/posts/foo-bar', $post->getCanonicalLink());
+	}
+
+	public function test_get_post_description_returns_post_description_when_set_in_front_matter()
+	{
+		$post = new MarkdownPost(['description' => 'This is a post description'], '', '', 'foo-bar');
+		$this->assertEquals('This is a post description', $post->getPostDescription());
+	}
+
+	public function test_get_post_description_returns_truncated_post_body_when_no_description_set_in_front_matter()
+	{
+		$post = new MarkdownPost([], 'This is a post body', '', 'foo-bar');
+		$this->assertEquals('This is a post body...', $post->getPostDescription());
+	}
+}
