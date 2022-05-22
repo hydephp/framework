62bc5c3c0abd77f82d0c589bfd60de7f8c5dda55
Caen De Silva <caen@desilva.se>
2022-05-22 21:18:32 +0200

    Change config key case to snake_case

--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -15,10 +15,10 @@
 		<strong class="mr-auto">
 			@if(Hyde::docsIndexPath() !== false)
 			<a href="{{ Hyde::relativeLink(Hyde::docsIndexPath(), $currentPage) }}">
-				{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+				{{ config('hyde.docs_sidebar_header_title', 'Documentation') }}
 			</a>
 			@else
-			{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+			{{ config('hyde.docs_sidebar_header_title', 'Documentation') }}
 			@endif
 		</strong>
         @include('hyde::components.navigation.theme-toggle-button')
@@ -35,10 +35,10 @@
 				<strong>
 					@if(Hyde::docsIndexPath() !== false)
 					<a href="{{ Hyde::relativeLink(Hyde::docsIndexPath(), $currentPage) }}">
-						{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+						{{ config('hyde.docs_sidebar_header_title', 'Documentation') }}
 					</a>
 					@else
-					{{ config('hyde.docsSidebarHeaderTitle', 'Documentation') }}
+					{{ config('hyde.docs_sidebar_header_title', 'Documentation') }}
 					@endif
 				</strong>
 				@include('hyde::components.navigation.theme-toggle-button')
