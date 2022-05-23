e54bf52a65ce0dc21931a6ddd515ec7c0f3db646
Caen De Silva <caen@desilva.se>
2022-05-23 22:45:27 +0200

    Merge test cases

--- tests/Feature/Commands/ValidateCommandTest.php
+++ tests/Feature/Commands/ValidateCommandTest.php
@@ -8,11 +8,6 @@ class ValidateCommandTest extends TestCase
 {
     public function test_validate_command_can_run()
     {
-        $this->artisan('validate')->assertExitCode(0);
-    }
-
-    public function test_it_prints_debug_information()
-    {
         $this->artisan('validate')
             ->expectsOutput('Running validation tests!')
             ->expectsOutput('All done!')
