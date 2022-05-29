e5b42c9dae6c909e8f4d36eeb8264fb6598df834
Caen De Silva <caen@desilva.se>
2022-05-29 09:56:51 +0200

    Add search input styling

--- resources/views/components/docs/search-page.blade.php
+++ resources/views/components/docs/search-page.blade.php
@@ -1,10 +1,24 @@
 <h1>Search the documentation</h1>
 
+<style>
+	#search-input {
+		font-size: 16px;
+		line-height: 1.5;
+		padding: 6px 10px;
+		background-color: #eee;
+		color: #000
+	}
+	.dark #search-input {
+		background-color: #333;
+		color: #fff;
+	}
+</style>
+
 <div id="hyde-search">
     <noscript>
         The search feature requires JavaScript to be enabled in your browser.
     </noscript>
-    <input type="search" name="search" id="search-input" placeholder="Search..." autocomplete="off">
+    <input type="search" name="search" id="search-input" placeholder="Search..." autocomplete="off" autofocus>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/hydesearch@0.2.1/dist/HydeSearch.min.js" defer></script>
