01034a457113363000d97aff298c56ed16b6e7f6
Caen De Silva <caen@desilva.se>
2022-05-29 12:09:46 +0200

    Move helper DOM element generations to functions

--- resources/views/components/docs/search-button.blade.php
+++ resources/views/components/docs/search-button.blade.php
@@ -77,27 +77,32 @@ function closeSearchMenu() {
 function openSearchMenu() {
 	searchMenu.setAttribute('open', '');
 
-	// Create a backdrop
-	const backdrop = document.createElement('div');
-	backdrop.id = 'searchMenuBackdrop';
-	backdrop.classList.add('backdrop', 'active');
-	backdrop.addEventListener('click', () => {
-		closeSearchMenu();
-	});
-	document.body.appendChild(backdrop);
+	createBackdrop();
+	createCloseButton();
 
-	// Create close button
-	const closeButton = document.createElement('button');
-	closeButton.id = 'searchMenuCloseButton';
-	closeButton.setAttribute('aria-label', 'Close search menu');
-	closeButton.addEventListener('click', () => {
-		closeSearchMenu();
-	});
-	closeButton.innerHTML = `<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-			<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-		</svg>`;
+	function createBackdrop() {
+		const backdrop = document.createElement('div');
+		backdrop.id = 'searchMenuBackdrop';
+		backdrop.classList.add('backdrop', 'active');
+		backdrop.addEventListener('click', () => {
+			closeSearchMenu();
+		});
+		document.body.appendChild(backdrop);
+	}
 
-	document.body.appendChild(closeButton);
+	function createCloseButton() {
+		const closeButton = document.createElement('button');
+		closeButton.id = 'searchMenuCloseButton';
+		closeButton.setAttribute('aria-label', 'Close search menu');
+		closeButton.addEventListener('click', () => {
+			closeSearchMenu();
+		});
+		closeButton.innerHTML = `<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+				<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+			</svg>`;
+
+		document.body.appendChild(closeButton);
+	}
 }
 
 document.addEventListener('keydown', (e) => {
