420f662a3040cc4c0f3f8e48d6a350686fb02803
Caen De Silva <caen@desilva.se>
2022-05-29 10:07:26 +0200

    Extract search widget to component

---
+++ resources/views/components/docs/search-widget.blade.php
@@ -0,0 +1,39 @@
+<style>
+	#search-input {
+		font-size: 16px;
+		line-height: 1.5;
+		padding: 6px 10px;
+		background-color: #eee;
+		color: #000
+	}
+	.hyde-search-context {
+		margin-bottom: 10px;
+	}
+	.dark #search-input {
+		background-color: #333;
+		color: #fff;
+	}
+	.dark .hyde-search-context mark.search-highlight {
+		background-color: rgba(255, 255, 0, 0.75);
+	}
+</style>
+
+<div id="hyde-search">
+    <noscript>
+        The search feature requires JavaScript to be enabled in your browser.
+    </noscript>
+    <input type="search" name="search" id="search-input" placeholder="Search..." autocomplete="off" autofocus>
+</div>
+
+@once('scripts')
+<script src="https://cdn.jsdelivr.net/npm/hydesearch@0.2.1/dist/HydeSearch.min.js" defer></script>
+
+<script>
+    window.addEventListener('load', function() {
+        const searchIndexLocation = 'search.json';
+        const Search = new HydeSearch(searchIndexLocation);
+
+        Search.init();
+    });
+</script>
+@endonce
