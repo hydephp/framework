a641ddfefbc9ec20166a53fb60c62d5e3cdad09a
Caen De Silva <caen@desilva.se>
2022-05-30 22:36:56 +0200

    Create "hidden" config option for default theme
    Hidden (as in not included in default config file) is that if falls under advanced and specific usage. If the default theme is changed, you would need to make sure all Blade views are compatible, and at that point I expect you have already published the views, and can then see the option there.

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -14,6 +14,6 @@
 
 @if(Hyde::features('darkmode'))
 {{-- Check the local storage for theme preference to avoid FOUC --}}
-<meta id="meta-color-scheme" name="color-scheme" content="light">
+<meta id="meta-color-scheme" name="color-scheme" content="{{ config('hyde.default_color_scheme', 'light') }}">
 <script>if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) { document.documentElement.classList.add('dark'); document.getElementById('meta-color-scheme').setAttribute('content', 'dark');} else { document.documentElement.classList.remove('dark') } </script>
 @endif
