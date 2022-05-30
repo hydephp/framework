332a992d5b5609e0c613a79f8826625b2b6a8723
Caen De Silva <caen@desilva.se>
2022-05-30 22:35:16 +0200

    Add color-scheme meta, fix #460

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -14,5 +14,6 @@
 
 @if(Hyde::features('darkmode'))
 {{-- Check the local storage for theme preference to avoid FOUC --}}
-<script>if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) { document.documentElement.classList.add('dark'); } else { document.documentElement.classList.remove('dark') } </script>
+<meta id="meta-color-scheme" name="color-scheme" content="light">
+<script>if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) { document.documentElement.classList.add('dark'); document.getElementById('meta-color-scheme').setAttribute('content', 'dark');} else { document.documentElement.classList.remove('dark') } </script>
 @endif
