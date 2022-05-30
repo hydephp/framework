3e28b1dcd91802596edf5dfa454fe2178432688f
Caen De Silva <caen@desilva.se>
2022-05-30 16:15:40 +0200

    Add !important to style override

--- resources/views/pages/documentation-search.blade.php
+++ resources/views/pages/documentation-search.blade.php
@@ -1,3 +1,3 @@
 <h1>Search the documentation site</h1>
-<style>#searchMenuButton{display:none;}#search-results{max-height:unset!important;}</style>
+<style>#searchMenuButton{display:none!important;}#search-results{max-height:unset!important;}</style>
 @include('hyde::components.docs.search-input')
