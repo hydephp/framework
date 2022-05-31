93e657ae8ac982cc0b351306deda03dda1951b67
Caen De Silva <caen@desilva.se>
2022-05-31 11:55:13 +0200

    Create the processor to compile Blade in Markdown

--- src/Services/BladeDownService.php
+++ src/Services/BladeDownService.php
@@ -3,6 +3,7 @@
 namespace Hyde\Framework\Services;
 
 use Hyde\Framework\Hyde;
+use Illuminate\Support\Facades\Blade;
 
 /**
  * Markdown Post Processor to render Laravel Blade within Markdown files.
@@ -28,7 +29,12 @@ class BladeDownService
 
     public function process(): self
     {
-        $this->output = $this->html;
+        $this->output = implode("\n", array_map(function ($line) {
+			if (str_starts_with($line, '[Blade]:')) {
+				return $this->processLine($line);
+			}
+            return $line;
+		}, explode("\n", $this->html)));
 
         return $this;
     }
@@ -42,4 +48,9 @@ class BladeDownService
     {
         return (new static($html))->process()->get();
 	}
+
+    protected function processLine(string $line): string
+    {
+        return Blade::render(substr($line, 8));
+    }
 }
