35fdba5709262656374970df418cf6c410390cb4
Caen De Silva <caen@desilva.se>
2022-05-31 21:40:03 +0200

    Remove superfluous tests

--- tests/Unit/Markdown/Shortcodes/InfoColoredBlockquoteTest.php
+++ tests/Unit/Markdown/Shortcodes/InfoColoredBlockquoteTest.php
@@ -10,15 +10,9 @@ use Tests\TestCase;
  */
 class InfoColoredBlockquoteTest extends TestCase
 {
-    public function test_signature()
-    {
-        $this->assertEquals('>info', InfoColoredBlockquote::signature());
-    }
-
     public function test_resolve()
     {
         $this->assertEquals('<blockquote class="info">foo</blockquote>',
             InfoColoredBlockquote::resolve('>info foo'));
-        $this->assertEquals('>foo', InfoColoredBlockquote::resolve('>foo'));
     }
 }
