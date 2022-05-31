fd4aadacf1421bf36d183677c5505a71259f6893
Caen De Silva <caen@desilva.se>
2022-05-31 11:12:04 +0200

    Create BladeDownService.php

---
+++ src/Services/BladeDownService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hyde\Framework\Services;
+
+use Hyde\Framework\Hyde;
+
+/**
+ * Markdown Post Processor to render Laravel Blade within Markdown files.
+ * 
+ * Works on a line-by-line basis by searching for a line starting with the directive.
+ * 
+ * @example: [Blade]: @include('path/to/view.blade.php')
+ * @example: [Blade]: @php(echo 'Hello World!')
+ * @example: [Blade]: {{ time() }}
+ */
+class BladeDownService
+{
+	
+}
