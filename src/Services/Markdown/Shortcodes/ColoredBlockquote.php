700e72257a758f23b870a73b9d5191976ab1e6bd
Caen De Silva <caen@desilva.se>
2022-05-31 21:00:42 +0200

    Create ColoredBlockquote.php

---
+++ src/Services/Markdown/Shortcodes/ColoredBlockquote.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hyde\Framework\Services\Markdown\Shortcodes;
+
+class ColoredBlockquote implements \Hyde\Framework\Contracts\MarkdownShortcodeContract
+{
+    public static function signature(): string
+    {
+        // TODO: Implement signature() method.
+    }
+
+    public static function resolve(string $input): string
+    {
+        // TODO: Implement resolve() method.
+    }
+}
