ae4b0dc24568483ed2be4330c290839f4382571a
Caen De Silva <caen@desilva.se>
2022-05-31 11:57:54 +0200

    Add base tests

--- tests/Feature/Services/BladeDownServiceTest.php
+++ tests/Feature/Services/BladeDownServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Services;
 
+use Hyde\Framework\Services\BladeDownService;
 use Tests\TestCase;
 
 /**
@@ -11,5 +12,31 @@ use Tests\TestCase;
  */
 class BladeDownServiceTest extends TestCase
 {
-    //
+    // Test it renders Blade echo syntax
+    public function test_it_renders_blade_echo_syntax()
+    {
+        $this->assertEquals('Hello World!', BladeDownService::render('[Blade]: {{ "Hello World!" }}'));
+    }
+    
+    // Test it renders Blade within multiline Markdown
+    public function test_it_renders_blade_within_multiline_markdown()
+    {
+        $this->assertEquals(
+            "Foo\nHello World!\nBar",
+
+            BladeDownService::render("Foo\n[Blade]: {{ 'Hello World!' }}\nBar")
+        );
+    }
+
+    // Test it renders Blade views
+    public function test_it_renders_blade_views()
+    {
+        file_put_contents(resource_path(
+            'views/hello.blade.php'
+        ), 'Hello World!');
+     
+        $this->assertEquals('Hello World!', BladeDownService::render('[Blade]: @include("hello")'));
+     
+        unlink(resource_path('views/hello.blade.php'));
+    }
 }
