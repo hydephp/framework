2968a58bda3ba0e90cf895c1e567abc5ebac75d8
Caen De Silva <caen@desilva.se>
2022-05-31 20:33:36 +0200

    Add PHPDoc

--- src/Contracts/MarkdownProcessorContract.php
+++ src/Contracts/MarkdownProcessorContract.php
@@ -4,5 +4,9 @@ namespace Hyde\Framework\Contracts;
 
 interface MarkdownProcessorContract
 {
+    /**
+     * @param string $input Markdown/HTML to be processed
+     * @return string $output Processed Markdown/HTML output
+     */
     public static function process(string $input): string;
 }
