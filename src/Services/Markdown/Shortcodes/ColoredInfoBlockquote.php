8d0cc421c4d9980b239f8d167ebee01a2f0114cc
Caen De Silva <caen@desilva.se>
2022-05-31 21:15:33 +0200

    Implement the shortcode

--- src/Services/Markdown/Shortcodes/ColoredInfoBlockquote.php
+++ src/Services/Markdown/Shortcodes/ColoredInfoBlockquote.php
@@ -4,17 +4,20 @@ namespace Hyde\Framework\Services\Markdown\Shortcodes;
 
 use Hyde\Framework\Contracts\MarkdownShortcodeContract;
 
+/**
+ * @see \Tests\Unit\Markdown\Shortcodes\ColoredInfoBlockquoteTest
+ */
 class ColoredInfoBlockquote implements MarkdownShortcodeContract
 {
     public static function signature(): string
     {
-        // TODO: Implement signature() method.
-        return '';
+        return '>info';
     }
 
     public static function resolve(string $input): string
     {
-        // TODO: Implement resolve() method.
-        return '';
+        return str_starts_with($input, static::signature())
+            ? '<blockquote class="info">' . trim(substr($input, strlen(static::signature())), ' ') . '</blockquote>'
+            : $input;
     }
 }
