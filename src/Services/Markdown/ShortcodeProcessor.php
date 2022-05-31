b19876a1d97e71fbd86008be01ced522989ebb2c
Caen De Silva <caen@desilva.se>
2022-05-31 20:37:10 +0200

    Sketch out the shortcode processor class

--- src/Services/Markdown/ShortcodeProcessor.php
+++ src/Services/Markdown/ShortcodeProcessor.php
@@ -6,6 +6,25 @@ use Hyde\Framework\Contracts\MarkdownProcessorContract;
 
 class ShortcodeProcessor implements MarkdownProcessorContract
 {
+    protected string $input;
+    protected string $output;
+
+    public function __construct(string $input)
+    {
+        $this->input = $input;
+    }
+
+    public function processInput(): self
+    {
+        $this->output = $this->input;
+        return $this;
+    }
+
+    public function getOutput(): string
+    {
+        return $this->output;
+    }
+
     public static function process(string $input): string
     {
         return (new static($input))->processInput()->getOutput();
