706b1294e141a657eccc238d9ef29a3074c19194
Caen De Silva <caen@desilva.se>
2022-06-01 17:36:39 +0200

    Cleanup code

--- src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
+++ src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
@@ -4,33 +4,30 @@ namespace Hyde\Framework\Services\Markdown;
 
 class AddFilepathLabelToCodeblockPostProcessor
 {
+    /**
+     * DOMDocument Proof of Concept
+     *
+     * @todo add ext-dom suggestion to composer.json
+     */
     public static function process(string $html): string
     {
-        // check if ext-dom extension is available
-        // @todo add ext-dom suggestion to composer.json
         if (! extension_loaded('dom')) {
             return $html;
         }
-        
-
-        // domdocument POC
 
         // Find all the code blocks
-        $codeBlocks = [];
         $dom = new \DOMDocument();
         $dom->loadHTML($html);
         $xpath = new \DOMXPath($dom);
         // Get query matching <pre><code>
         $query = '//pre/code';
         $codeBlocks = $xpath->query($query);
-        
 
         // Add the filepath label to each code block
         foreach ($codeBlocks as $codeBlock) {
-            // Get the filepath which is the everything after `// Filepath:` on the first line of the code block
+            // Get the filepath which is everything after `// Filepath:` on the first line of the code block
             $filepath = trim(explode("\n", $codeBlock->textContent)[0]);
             $filepath = substr($filepath, strpos($filepath, ':') + 1);
-
 
             // Remove the first line of the code block text
             $text = explode("\n", $codeBlock->textContent);
