9e1954ea6f262ce3fcb358eb1c68d870a4f73b25
Caen De Silva <caen@desilva.se>
2022-06-01 14:51:57 +0200

    Update the test to new system

--- tests/Feature/ColoredBlockquoteShortcodesTest.php
+++ tests/Feature/ColoredBlockquoteShortcodesTest.php
@@ -2,47 +2,26 @@
 
 namespace Tests\Feature;
 
+use Hyde\Framework\Services\Markdown\Shortcodes\AbstractColoredBlockquote;
 use Tests\TestCase;
-
-use Hyde\Framework\Services\Markdown\Shortcodes\DangerColoredBlockquote;
-use Hyde\Framework\Services\Markdown\Shortcodes\SuccessColoredBlockquote;
-use Hyde\Framework\Services\Markdown\Shortcodes\InfoColoredBlockquote;
-use Hyde\Framework\Services\Markdown\Shortcodes\WarningColoredBlockquote;
-
 
 /**
  * Class ColoredBlockquoteShortcodesTest.
  * 
  * @covers \Hyde\Framework\Services\Markdown\Shortcodes\AbstractColoredBlockquote
- * @covers \Hyde\Framework\Services\Markdown\Shortcodes\DangerColoredBlockquote
- * @covers \Hyde\Framework\Services\Markdown\Shortcodes\SuccessColoredBlockquote
- * @covers \Hyde\Framework\Services\Markdown\Shortcodes\InfoColoredBlockquote
- * @covers \Hyde\Framework\Services\Markdown\Shortcodes\WarningColoredBlockquote
  */
 class ColoredBlockquoteShortcodesTest extends TestCase
 {
-    public function test_resolve_danger()
+    public function test_resolve_method()
     {
-        $this->assertEquals('<blockquote class="danger">foo</blockquote>',
-            DangerColoredBlockquote::resolve('>danger foo'));
+        $this->assertEquals('<blockquote class="color">foo</blockquote>',
+            AbstractColoredBlockquote::resolve('>color foo'));
     }
 
-    public function test_resolve_success()
+    public function test_get_method()
     {
-        $this->assertEquals('<blockquote class="success">foo</blockquote>',
-            SuccessColoredBlockquote::resolve('>success foo'));
+        $this->assertCount(4, AbstractColoredBlockquote::get());
+        $this->assertContainsOnlyInstancesOf(AbstractColoredBlockquote::class,
+            AbstractColoredBlockquote::get());
     }
-
-    public function test_resolve_info()
-    {
-        $this->assertEquals('<blockquote class="info">foo</blockquote>',
-            InfoColoredBlockquote::resolve('>info foo'));
-    }
-
-    public function test_resolve_warning()
-    {
-        $this->assertEquals('<blockquote class="warning">foo</blockquote>',
-            WarningColoredBlockquote::resolve('>warning foo'));
-    }
-
 }
