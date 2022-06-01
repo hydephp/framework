4bfcbcac7233f5ad9f9b9fa25b891e94d95606af
Caen De Silva <caen@desilva.se>
2022-06-01 15:44:22 +0200

    Replace array_map with foreach

--- src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
+++ src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
@@ -6,12 +6,14 @@ class AddFilepathLabelToCodeblockPostProcessor
 {
     public static function process(string $html): string
     {
-        return implode("\n", array_map(function ($line) {
+        $lines = explode("\n", $html);        
+
+        foreach ($lines as $index => $line) {
             if (str_starts_with(strtolower($line), '<pre><code class="language-markdown">// filepath: ')) {
-                $line = str_replace('// Filepath: ', '<small class="filepath"><span class="sr-only">Filepath: </span>', $line);
-                return $line . '</small>';
+                $lines[$index] = str_replace('// Filepath: ', '<small class="filepath"><span class="sr-only">Filepath: </span>', $line) . '</small>';
             }
-            return $line;
-        }, explode("\n", $html)));
+        }
+
+        return implode("\n", $lines);
     }
 }
