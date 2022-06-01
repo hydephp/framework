71506fec43cdbaabdf4f6fcbe4001140aa2dc896
Caen De Silva <caen@desilva.se>
2022-06-01 17:33:13 +0200

    Rewrite to a domdocument based POC

--- src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
+++ src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
@@ -6,14 +6,40 @@ class AddFilepathLabelToCodeblockPostProcessor
 {
     public static function process(string $html): string
     {
-        $lines = explode("\n", $html);        
+        // domdocument POC
 
-        foreach ($lines as $index => $line) {
-            if (str_starts_with(strtolower($line), '<pre><code class="language-markdown">// filepath: ')) {
-                $lines[$index] = str_replace('// Filepath: ', '<small class="filepath"><span class="sr-only">Filepath: </span>', $line) . '</small>';
-            }
+        // Find all the code blocks
+        $codeBlocks = [];
+        $dom = new \DOMDocument();
+        $dom->loadHTML($html);
+        $xpath = new \DOMXPath($dom);
+        // Get query matching <pre><code>
+        $query = '//pre/code';
+        $codeBlocks = $xpath->query($query);
+        
+
+        // Add the filepath label to each code block
+        foreach ($codeBlocks as $codeBlock) {
+            // Get the filepath which is the everything after `// Filepath:` on the first line of the code block
+            $filepath = trim(explode("\n", $codeBlock->textContent)[0]);
+            $filepath = substr($filepath, strpos($filepath, ':') + 1);
+
+
+            // Remove the first line of the code block text
+            $text = explode("\n", $codeBlock->textContent);
+            array_shift($text);
+            array_shift($text);
+            $codeBlock->textContent = implode("\n", $text);
+
+            // Create the filepath label `<small>$filepath<small>` element
+            $filepathLabel = $dom->createElement('small', $filepath);
+            // Add the class `filepath` to the filepath label
+            $filepathLabel->setAttribute('class', 'filepath');
+
+            // Prepend the filepath label to the first child of the code block
+            $codeBlock->insertBefore($filepathLabel, $codeBlock->firstChild);
         }
 
-        return implode("\n", $lines);
+        return $dom->saveHTML();
     }
 }
