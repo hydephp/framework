bdba27386df05a46ca27071601aed1f6f3f00b59
Caen De Silva <caen@desilva.se>
2022-06-01 20:27:29 +0200

    Use the same static transformation instead of DOM

--- src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
+++ src/Services/Markdown/AddFilepathLabelToCodeblockPostProcessor.php
@@ -3,11 +3,6 @@
 namespace Hyde\Framework\Services\Markdown;
 
 /**
- * DOMDocument Proof of Concept
- *
- * @note This does not work with Torchlight highlighted code blocks.
- *
- * @todo Add ext-dom suggestion to composer.json
  * @todo Add config option to enable/disable this processor
  */
 class AddFilepathLabelToCodeblockPostProcessor
@@ -17,8 +12,28 @@ class AddFilepathLabelToCodeblockPostProcessor
         $torchlightKey = '<!-- Syntax highlighted by torchlight.dev -->';
         $template = '<small class="filepath"><span class="sr-only">Filepath: </span>%s</small>';
 
-        if (! str_contains($html, $torchlightKey)) {
-            return (new static($html))->run();
+        if (str_contains($html, $torchlightKey)) {
+
+            $lines = explode("\n", $html);
+
+            foreach ($lines as $index => $line) {
+                if (str_starts_with($line, '<!-- HYDE[Filepath]')) {
+                    $path = trim(
+                        str_replace('-->', '', str_replace(
+                            '<!-- HYDE[Filepath]', '', $line
+                        ))
+                    );
+                    unset($lines[$index]);
+                    $codeBlockLine = $index + 1;
+                    $lines[$codeBlockLine] = str_replace(
+                        $torchlightKey,
+                        $torchlightKey . sprintf($template, $path, $lines[$codeBlockLine]),
+                        $lines[$codeBlockLine]
+                        );
+                }
+            }
+
+            return implode("\n", $lines);
         }
 
         $lines = explode("\n", $html);
@@ -31,16 +46,22 @@ class AddFilepathLabelToCodeblockPostProcessor
                     ))
                 );
                 unset($lines[$index]);
+
                 $codeBlockLine = $index + 1;
-                $lines[$codeBlockLine] = str_replace(
-                    $torchlightKey,
-                    $torchlightKey . sprintf($template, $path, $lines[$codeBlockLine]),
-                     $lines[$codeBlockLine]
-                    );
+
+                $label = sprintf($template, $path, $lines[$codeBlockLine]);
+
+                // Insert the label after the '<pre><code class="language-*">' using regex
+                $lines[$codeBlockLine] = preg_replace(
+                    '/<pre><code class="language-(.*?)">/',
+                    '<pre><code class="language-$1">' . $label,
+                    $lines[$codeBlockLine]
+                );
             }
         }
 
         return implode("\n", $lines);
+
     }
 
     public static function preprocess(string $markdown): string
@@ -75,59 +96,6 @@ class AddFilepathLabelToCodeblockPostProcessor
         '# filepath ',
         '# Filepath ',
     ];
-
-    protected string $html;
-
-    public function __construct(string $html) {
-        $this->html = $html;
-
-    }
-
-    public function run(): string
-    {
-        if (! extension_loaded('dom')) {
-            return $this->html;
-        }
-
-        // Find all the code blocks
-        $dom = new \DOMDocument();
-        $dom->loadHTML($this->html);
-        $xpath = new \DOMXPath($dom);
-        // Get query matching <pre><code>
-        $query = '//pre/code';
-        $codeBlocks = $xpath->query($query);
-
-        // Add the filepath label to each code block
-        foreach ($codeBlocks as $codeBlock) {
-            // Get the first line (everything before the first newline in $codeBlock->textContent)
-            $firstLine = strtok($codeBlock->textContent, "\n");
-            
-            // Check if it matches any of the patterns
-            if (static::lineMatchesPattern($firstLine)) {
-                // Get the filepath
-                $filepath = trim(str_replace(static::$patterns, '', $firstLine));
-
-                // Remove the first line of the code block text
-                $codeBlock->textContent = substr($codeBlock->textContent,
-                    strpos($codeBlock->textContent, "\n") + 2);
-
-                // Create the filepath label `<small>$filepath<small>` element
-                $filepathLabel = $dom->createElement('small', $filepath);
-                // Add the class `filepath` to the filepath label
-                $filepathLabel->setAttribute('class', 'filepath');
-                $filepathLabel->setAttribute('title', 'Filepath');
-                // Add screen reader text
-                $screenReaderText = $dom->createElement('span', 'Filepath: ');
-                $screenReaderText->setAttribute('class', 'sr-only');
-                $filepathLabel->insertBefore($screenReaderText, $filepathLabel->firstChild);
-
-                // Prepend the filepath label to the first child of the code block
-                $codeBlock->insertBefore($filepathLabel, $codeBlock->firstChild);
-            }
-        }
-
-        return $dom->saveHTML();
-    }
 
     protected static function lineMatchesPattern(string $line): bool
     {
