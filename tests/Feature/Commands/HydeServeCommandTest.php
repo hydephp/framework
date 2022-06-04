39de6e323b5d421eb5f27bb0e1b2ca6fa0d8355e
Caen De Silva <caen@desilva.se>
2022-06-04 16:39:41 +0200

    Remove output assertion for informational string

--- tests/Feature/Commands/HydeServeCommandTest.php
+++ tests/Feature/Commands/HydeServeCommandTest.php
@@ -13,7 +13,6 @@ class HydeServeCommandTest extends TestCase
     {
         $this->artisan('serve')
             ->expectsOutput('Starting the server... Press Ctrl+C to stop')
-            ->expectsOutput('This feature is experimental. Please report any issues on GitHub.')
             ->assertExitCode(0);
     }
 }
