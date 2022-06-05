19a09c9986aaa79ba92da0618d475c54619ba60f
Caen De Silva <caen@desilva.se>
2022-06-05 18:58:57 +0200

    Add static::all() helper

--- src/Contracts/PageContract.php
+++ src/Contracts/PageContract.php
@@ -2,7 +2,14 @@
 
 namespace Hyde\Framework\Contracts;
 
+use Illuminate\Support\Collection;
+
 interface PageContract
 {
-    //
+    /**
+     * Get a collection of all pages, parsed into page models.
+     * @return \Illuminate\Support\Collection<\Hyde\Framework\Contracts\PageContract>
+     * @see \Tests\Unit\PageModelGetHelperTest
+     */
+    public static function all(): Collection;
 }
