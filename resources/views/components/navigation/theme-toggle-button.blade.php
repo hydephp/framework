7d6b998aad6de574f2c84b2046c6d0402ce1c4ba
Caen De Silva <caen@desilva.se>
2022-06-06 19:57:52 +0200

    Handle the deprecation

--- resources/views/components/navigation/theme-toggle-button.blade.php
+++ resources/views/components/navigation/theme-toggle-button.blade.php
@@ -1,4 +1,4 @@
-@if(Hyde::features('darkmode'))
+@if(Hyde::hasFeature('darkmode'))
 <button class="theme-toggle-button flex items-center px-3 py-1 hover:text-gray-700 dark:text-gray-200" title="Toggle theme">
 	<span class="sr-only">Toggle dark theme</span>
 	<figure class="theme-icon" role="presentation"></figure>
