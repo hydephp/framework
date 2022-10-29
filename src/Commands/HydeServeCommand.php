5a0621f7076b9ad1fc48cfc7c411f9ca36eed027
rgasch <rgasch@gmail.com>
2022-10-29 23:14:52 +0200

    Made port default to value specific in config file or in .env

--- src/Commands/HydeServeCommand.php
+++ src/Commands/HydeServeCommand.php
@@ -17,7 +17,7 @@ class HydeServeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'serve {--port=8080} {--host=localhost}';
+    protected $signature = 'serve {--port=} {--host=localhost}';
 
     /**
      * The description of the command.
@@ -35,8 +35,12 @@ class HydeServeCommand extends Command
     {
         $this->line('<info>Starting the HydeRC server...</info> Press Ctrl+C to stop');
 
-        $port = $this->option('port');
         $host = $this->option('host');
+        $port = $this->option('port');
+        if (!$port) {
+            $port = config('hyde.server.port', 8080);
+        }
+
         $command = "php -S $host:$port ".Hyde::path('vendor/hyde/realtime-compiler/bin/server.php');
         if (app()->environment('testing')) {
             $command = 'echo '.$command;
