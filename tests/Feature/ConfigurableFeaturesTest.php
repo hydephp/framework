648f9f178bd19d27701bbe84623fafe9222d6707
Caen De Silva <caen@desilva.se>
2024-06-30 14:49:17 +0200

    Revert "Add a config option to disable theme toggle buttons to automatically use browser settings"

--- tests/Feature/ConfigurableFeaturesTest.php
+++ tests/Feature/ConfigurableFeaturesTest.php
@@ -58,43 +58,6 @@ class ConfigurableFeaturesTest extends TestCase
         $this->assertFalse(Features::sitemap());
     }
 
-    public function testHasThemeToggleButtonsReturnsTrueWhenDarkmodeEnabledAndConfigTrue()
-    {
-        // Enable dark mode and set hyde.theme_toggle_buttons config option to true
-        Features::mock('darkmode', true);
-        config(['hyde.theme_toggle_buttons' => true]);
-
-        $this->assertTrue(Features::hasThemeToggleButtons());
-    }
-
-    public function testHasThemeToggleButtonsReturnsFalseWhenDarkmodeDisabled()
-    {
-        // Disable dark mode
-        Features::mock('darkmode', false);
-        // It doesn't matter what the config value is here
-
-        $this->assertFalse(Features::hasThemeToggleButtons());
-    }
-
-    public function testHasThemeToggleButtonsReturnsFalseWhenConfigFalse()
-    {
-        // Enable dark mode
-        Features::mock('darkmode', true);
-        // Set hyde.theme_toggle_buttons config option to false
-        config(['hyde.theme_toggle_buttons' => false]);
-
-        $this->assertFalse(Features::hasThemeToggleButtons());
-    }
-
-    public function testHasThemeToggleButtonsReturnsTrueWhenDarkmodeEnabledAndConfigNotSet()
-    {
-        // Enable dark mode
-        Features::mock('darkmode', true);
-        // Config option not set, default value assumed to be true
-
-        $this->assertTrue(Features::hasThemeToggleButtons());
-    }
-
     public function testToArrayMethodReturnsMethodArray()
     {
         $array = (new Features)->toArray();
@@ -117,11 +80,10 @@ class ConfigurableFeaturesTest extends TestCase
         $this->assertArrayHasKey('markdown-pages', $array);
         $this->assertArrayHasKey('documentation-pages', $array);
         $this->assertArrayHasKey('darkmode', $array);
-        $this->assertArrayHasKey('theme-toggle-buttons', $array);
         $this->assertArrayHasKey('documentation-search', $array);
         $this->assertArrayHasKey('torchlight', $array);
 
-        $this->assertCount(9, $array);
+        $this->assertCount(8, $array);
     }
 
     public function testFeaturesCanBeMocked()
