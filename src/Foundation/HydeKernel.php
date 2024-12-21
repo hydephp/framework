e6790c07cd047fed9487a3ffaa4cf761c2895cc4
Caen De Silva <caen@desilva.se>
2024-12-21 13:06:07 +0000

    Merge pull request #2068 from hydephp/framework-v1.7.4
    Framework version v1.7.4 https://github.com/hydephp/develop/commit/6a13d76adbef297aa0c93cb1c9080b927b28d957

--- src/Foundation/HydeKernel.php
+++ src/Foundation/HydeKernel.php
@@ -58,7 +58,7 @@ class HydeKernel implements SerializableContract
     use Serializable;
     use Macroable;
 
-    final public const VERSION = '1.7.3';
+    final public const VERSION = '1.7.4';
 
     protected static self $instance;
 
