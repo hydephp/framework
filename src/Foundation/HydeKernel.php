c5faf11f8377c0064d5f9738fa58e46ed10fbf16
Caen De Silva <caen@desilva.se>
2025-05-24 22:37:51 +0200

    HydePHP v2 Release Candidate One

--- src/Foundation/HydeKernel.php
+++ src/Foundation/HydeKernel.php
@@ -50,7 +50,7 @@ class HydeKernel implements SerializableContract
     use Serializable;
     use Macroable;
 
-    final public const VERSION = '1.8.0';
+    final public const VERSION = '2.0.0';
 
     protected static self $instance;
 
