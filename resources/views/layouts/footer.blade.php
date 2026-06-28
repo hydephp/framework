9faea557a6a6e9464bdb776a0548dae998a9d6c7
Emma De Silva <emma@desilva.se>
2026-06-28 13:51:30 +0000

    Merge pull request #2458 from hydephp/go-to-top-button-improvements
    Update scroll to top button to smooth scroll to top https://github.com/hydephp/develop/commit/d5f32a67715154eec078348d9d14a6d524a65209

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="prose dark:prose-invert text-center mx-auto">
             {!! \Hyde\Support\Includes::markdown('footer', config('hyde.footer', 'Site proudly built with [HydePHP](https://github.com/hydephp/hyde) 🎩')) !!}
         </div>
-        <a href="#app" aria-label="Go to top of page" class="float-right">
+        <a href="#app" aria-label="Go to top of page" class="float-right" @click.prevent="document.querySelector('#app').scrollIntoView({ behavior: 'smooth' })">
             <button title="Scroll to top">
                 <svg width="1.5rem" height="1.5rem" role="presentation" class="fill-current text-gray-500 h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" />
